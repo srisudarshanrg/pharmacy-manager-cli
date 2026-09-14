9eb95beede204b7fddf55af5d40d53c8a395781e
R Sri Sudarshan <srisudarshanrg@gmail.com>
2026-09-14 17:02:59 +0530

    base cli complete

--- run.py
+++ run.py
@@ -1,27 +1,65 @@
-from db import db, cur
-from db_create import medicine_catalog, customer_info, purchase_record
+from datetime import datetime
+from functions import add_to_medicine_catalog, delete_from_medicine_catalog, purchase_medicine, add_to_customers, get_id_from_customer_name, get_customer_purchase_history
 
-cur.execute(medicine_catalog)
-db.commit()
+options = int(input("Are you an admin or a customer:\n" \
+"(1) Admin\n" \
+"(2) Customer\n"
+"Enter Option: "))
 
-cur.execute(purchase_record)
-db.commit()
+if options == 1:
+    admin_options = int(input("\nChoose one of the following options:\n" \
+    "(1) add a medicine\n" \
+    "(2) remove a medicine\n" \
+    "(3) view customer records\n"
+    "Enter Option: "))
 
-cur.execute(customer_info)
-db.commit()
+    if admin_options == 1:
+        add_medicine = input("\nSpecifiy the following fields:\n" \
+        "- medicine name\n" \
+        "- expire\n" \
+        "- quantity\n" \
+        "- price\n" \
+        "separated by spaces:\n")
 
-# test
-query = "SELECT * FROM medicines"
-cur.execute(query)
-rows = cur.fetchall()
+        medicine_description_list = add_medicine.split(" ")
 
-if not rows:
-    print("no rows available")
-else:
-    for row in rows:
-        print(row)
+        medicine_description = {
+            "medicine": medicine_description_list[0],
+            "expiry":  datetime.strptime(medicine_description_list[1], "%d/%m/%Y"),
+            "quantity": medicine_description_list[2],
+            "price": int(medicine_description_list[3])
+        }
 
-import cli
+        status = add_to_medicine_catalog(medicine_name=medicine_description["medicine"], medicine=medicine_description)
 
-cur.close()
-db.close()
+    elif admin_options == 2:
+        medicine_name = input("Enter medicine name: ")
+        status = delete_from_medicine_catalog(medicine_name)
+        print(status)
+
+    elif admin_options == 3:
+        customer_details = input("Enter customer name and phone number separated by spaces: ")
+        customer_details = customer_details.split(" ")
+
+        status = get_customer_purchase_history(customer_details[0], int(customer_details[1]))
+
+        print("|  ID  |  NAME  |  MEDICINE  |  EXPIRY  |  QUANTITY  |  PRICE  |/n")
+        for s in status:
+            print(f"|  {s["id"]}  |  {s["name"]}  |  {s["medicine"]}  |  {s["expiry"]}  |  {s["quantity"]}  |  {s["price"]} rs")    
+
+    else:
+        print("Valid entries are 1, 2 or 3. Enter one of 1, 2 or 3 only")
+
+elif options == 2:
+    customer_options = int(input("Choose on of the following\n" \
+    "(1) Purchase Medicine\n" \
+    "Enter option: "))
+
+    if customer_options == 1:
+        purchase_medicine_input = input("Enter your name, phone number and medicine name separated by spaces ")
+
+        purchase_medicine_list = purchase_medicine_input.split(" ")
+        status = purchase_medicine(purchase_medicine_list[0], purchase_medicine_list[1], purchase_medicine_list[2])
+
+        print(status)
+
